c1ef0f393d91ff5dc44903871d4d8a80cea87ffb
pramilaniroshan <pramilaniroshan@gmail.com>
2022-03-24 12:10:57 +0530

    app colors setup

---
+++ lib/config/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFFC4C4C4);
+  static const secondary = Color(0xFF006590);
+  static const lightBackground = Color(0xFFF5F5F5);
+  static const lightBlack = Color(0xFF434343);
+  static const white = Colors.white;
+  static const black = Colors.black;
+}
