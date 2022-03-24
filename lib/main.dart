91c69b1a554323df0f3f7d6ca3a09b762f254d0a
pramilaniroshan <pramilaniroshan@gmail.com>
2022-03-24 12:12:29 +0530

    shared_preferences added for local storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
-import 'package:emedassistantmobile/config/colors.dart';
-import 'package:emedassistantmobile/config/constants.dart';
-import 'package:emedassistantmobile/screens/login.dart';
+import 'package:emedassistantmobile/config/app_colors.dart';
+import 'package:emedassistantmobile/screens/auth/sign_in_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'dart:convert' show json, base64, ascii;
 
 
 void main() {
   runApp( MaterialApp(theme :ThemeData(
-    primarySwatch: Colors.yellow,
-  ),home: LoginScreen(),),);
+    primarySwatch: Colors.blue,
+  ),home:const SignInScreen()),);
   
 }
