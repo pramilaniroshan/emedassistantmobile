355779500e5739b74cae43e7661d679780b7bfc7
pramilaniroshan <pramilaniroshan@gmail.com>
2022-05-17 18:17:53 +0530

    app colors and images added

--- lib/config/app_colors.dart
+++ lib/config/app_colors.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Color(0xFFC4C4C4);
-  static const secondary = Color(0xFF006590);
-  static const lightBackground = Color(0xFFF5F5F5);
-  static const lightBlack = Color(0xFF434343);
-  static const white = Colors.white;
-  static const black = Colors.black;
+  static const Color primary = Color(0xFFC4C4C4);
+  static const Color secondary = Color(0xFF006590);
+  static const Color lightBackground = Color(0xFFF5F5F5);
+  static const Color lightBlack = Color(0xFF434343);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color lightBlue = Color(0xff108ee9);
+  static const Color redColor = Color(0xffD22B27);
 }
