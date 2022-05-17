8b9102c27bf9ccd05a15b175a589cbd96e2baed3
pramilaniroshan <pramilaniroshan@gmail.com>
2022-05-17 18:19:07 +0530

    spalsh screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,36 @@
 import 'package:emedassistantmobile/config/app_colors.dart';
-import 'package:emedassistantmobile/screens/auth/sign_in_screen.dart';
+import 'package:emedassistantmobile/config/app_images.dart';
+import 'package:emedassistantmobile/screens/auth/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 
 void main() {
-  runApp( MaterialApp(theme :ThemeData(
-    primarySwatch: Colors.blue,
-  ),home:const SignInScreen()),);
-  
+ runApp(const MyApp());  
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'eMed App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Roboto',
+      ),
+      //home: CreateProfileScreen(),
+      home: AnimatedSplashScreen(
+            duration: 3000,
+            splash: SvgPicture.asset(AppImages.eMedLogo,height: 50, width: 50,
+      fit: BoxFit.scaleDown),
+            nextScreen: HomeScreen(),
+            splashTransition: SplashTransition.fadeTransition,
+            backgroundColor: Colors.white));
+  }
 }
