d18af5c3f787009f4c0f1bf3050674e41a77c2f2
pramilaniroshan <pramilaniroshan@gmail.com>
2022-07-05 07:34:46 +0530

    QR reader added

--- lib/screens/scan_qr/scan_qr_screen.dart
+++ lib/screens/scan_qr/scan_qr_screen.dart
@@ -1,125 +1,170 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-import 'package:emedassistantmobile/config/app_colors.dart';
-import 'package:emedassistantmobile/config/app_images.dart';
-
-class ScanQrScreen extends StatefulWidget {
-  const ScanQrScreen({Key? key}) : super(key: key);
+class QRViewExample extends StatefulWidget {
+  const QRViewExample({Key? key}) : super(key: key);
 
   @override
-  State<ScanQrScreen> createState() => _ScanQrScreenState();
+  State<StatefulWidget> createState() => _QRViewExampleState();
 }
 
-class _ScanQrScreenState extends State<ScanQrScreen> {
+class _QRViewExampleState extends State<QRViewExample> {
+  Barcode? result;
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
+
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-        const SystemUiOverlayStyle(
-          statusBarColor: Colors.black,
-          statusBarIconBrightness: Brightness.light,
-        )
-    );
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: AppColors.lightBackground,
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Image.asset(AppImages.backTopImage),
-            Padding(
-              padding: const EdgeInsets.only(left: 40.0, right: 20.0, top: 12.0),
+      body: Column(
+        children: <Widget>[
+          Expanded(flex: 4, child: _buildQrView(context)),
+          Expanded(
+            flex: 1,
+            child: FittedBox(
+              fit: BoxFit.contain,
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  if (result != null)
+                    Text(
+                        'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
+                  else
+                    const Text('Scan a code'),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset(AppImages.eMedLogo, color: AppColors.white,
-                      width: width * 0.3,
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                            onPressed: () async {
+                              await controller?.toggleFlash();
+                              setState(() {});
+                            },
+                            child: FutureBuilder(
+                              future: controller?.getFlashStatus(),
+                              builder: (context, snapshot) {
+                                return Text('Flash: ${snapshot.data}');
+                              },
+                            )),
                       ),
-                      const SizedBox(width: 16.0),
-                      const Text('Your medical assistant solution',
-                      style: TextStyle(
-                        fontSize: 12.0,
-                        color: AppColors.white,
-                      ),
-                      ),
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                            onPressed: () async {
+                              await controller?.flipCamera();
+                              setState(() {});
+                            },
+                            child: FutureBuilder(
+                              future: controller?.getCameraInfo(),
+                              builder: (context, snapshot) {
+                                if (snapshot.data != null) {
+                                  return Text(
+                                      'Camera facing ${describeEnum(snapshot.data!)}');
+                                } else {
+                                  return const Text('loading');
+                                }
+                              },
+                            )),
+                      )
                     ],
                   ),
-
-                  /// doctor name
-                  const SizedBox(height: 20.0),
-                  const Text('Dr. Imesha Basnayaka',
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      color: AppColors.white,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  const SizedBox(height: 3.0),
-                  const Text('Dermatologists - Pediatricians',
-                    style: TextStyle(
-                      fontSize: 10.0,
-                      color: AppColors.white,
-                    ),
-                  ),
-
-                  /// specialist
-                  const SizedBox(height: 10.0),
-                  const Text('Lady Ridgway',
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      color: AppColors.white,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  const SizedBox(height: 3.0),
-                  const Text('Dr Mawatha, Colombo 0800, Sri Lanka',
-                    style: TextStyle(
-                      fontSize: 10.0,
-                      color: AppColors.white,
-                    ),
-                  ),
-
-                  /// qr code image
-                  const SizedBox(height: 20.0),
-                  Image.asset(AppImages.qrCodeImage,
-                    width: width,
-                    height: height * 0.44,
-                    fit: BoxFit.fill,
-                  ),
-
-                  /// use camera text
-                  const SizedBox(height: 20.0),
-                  const Text('Use the camera of your mobile phone to scan the QRcode above '
-                      'to easily book an appointment.',
-                    style: TextStyle(
-                      fontSize: 13.0,
-                      color: AppColors.lightBlack,
-                    ),
-                  ),
-
-                  /// powered by text
-                  const SizedBox(height: 20.0),
-                  const Align(
-                    alignment: Alignment.centerRight,
-                    child: Text('Powered by emedassistant.com',
-                      style: TextStyle(
-                        fontSize: 10.0,
-                        color: AppColors.lightBlack,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            await controller?.pauseCamera();
+                          },
+                          child: const Text('pause',
+                              style: TextStyle(fontSize: 20)),
+                        ),
                       ),
-                    ),
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            await controller?.resumeCamera();
+                          },
+                          child: const Text('resume',
+                              style: TextStyle(fontSize: 20)),
+                        ),
+                      )
+                    ],
                   ),
                 ],
               ),
             ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
+
+  Widget _buildQrView(BuildContext context) {
+    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+    // To ensure the Scanner view is properly sizes after rotation
+    // we need to listen for Flutter SizeChanged notification and update controller
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.red,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('no Permission')),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+  //flutter.compileSdkVersion
 }
